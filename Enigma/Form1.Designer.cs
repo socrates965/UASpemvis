81a82c3a3c94bfb45ade8740ff066d990648001d
Socrates Sullivan Riadi Putra <socrates.sullivan@gmail.com>
2018-08-12 17:10:58 +0700

    running enigma machine

--- Enigma/Form1.Designer.cs
+++ Enigma/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -39,6 +38,8 @@
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
@@ -46,103 +47,145 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(92, 148);
+            this.textBox1.Location = new System.Drawing.Point(122, 140);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(248, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(92, 223);
+            this.textBox2.Location = new System.Drawing.Point(122, 203);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(248, 22);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(92, 306);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Location = new System.Drawing.Point(40, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(76, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Slow Rotor";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 24);
+            this.label2.Location = new System.Drawing.Point(153, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(96, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Medium Rotor";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(260, 24);
+            this.label3.Location = new System.Drawing.Point(296, 30);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.Size = new System.Drawing.Size(74, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Fast Rotor";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 155);
+            this.label4.Location = new System.Drawing.Point(27, 149);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.Size = new System.Drawing.Size(39, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Input";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 223);
+            this.label5.Location = new System.Drawing.Point(27, 203);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.Size = new System.Drawing.Size(51, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "Output";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(33, 61);
+            this.numericUpDown1.Location = new System.Drawing.Point(44, 75);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 22);
             this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(149, 61);
+            this.numericUpDown2.Location = new System.Drawing.Point(173, 75);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(53, 22);
             this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(263, 61);
+            this.numericUpDown3.Location = new System.Drawing.Point(300, 75);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown3.Size = new System.Drawing.Size(53, 22);
             this.numericUpDown3.TabIndex = 13;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 257);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 257);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(684, 492);
+            this.ClientSize = new System.Drawing.Size(912, 606);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.numericUpDown1);
@@ -151,9 +194,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -169,7 +212,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -178,6 +220,8 @@
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Enigma/Form1.cs
+++ Enigma/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,17 +13,44 @@ namespace Enigma
 {
     public partial class Form1 : Form
     {
-        Class1 class1 = new Class1();
-        int rotor1, rotor2, rotor3;
+        public Regex ralpha = new Regex("[A-Z]");
+        public int i, ii, iii, iv;
+        public int n;
+        public int l = 0;
+        public bool backspace = false;
+        
+        public static string sModel = "Enigma I";
+        public static string sRotor1 = "I";
+        public static string sRotor2 = "II";
+        public static string sRotor3 = "III";
+        public static string sRotor4 = "";
+        public static string sReflector = "B";
+        public int[,] ringgeschichte = new int[5, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        public int geschichte = 0;
+        //B style reflector
+        public static Rotor reflector1 = new Rotor("YRUHQSLDPXNGOKMIEBFZCWVJAT");
+        //public string input, output;
+
+        public static int notch1 = 0;
+        public static int notch2 = 5;
+        public static int notch3 = 22;
+        public static int notch1second = 0;
+        public static int notch2second = 52;
+        public static int notch3second = 52;
+
+        public static string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string shidden = "";
+        public static Rotor rotor1 = new Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ");
+        public static Rotor rotor2 = new Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE");
+        public static Rotor rotor3 = new Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO");
+        public static Rotor rotor4 = new Rotor("ESOVPZJAYQUIRHXLNFTGKDCMWB");
+
+        public static string[] steckerbrett = new string[2] { "", "" };
+        public static int[] ringstellung = new int[4] { 0, 0, 0, 0 };
+
         public Form1()
         {
             InitializeComponent();
-            
-            comboBox1.SelectedIndex = 0;
-            comboBox2.SelectedIndex = 0;
-            comboBox3.SelectedIndex = 0;
-            
-            //machine.SetCode(rotor1, rotor2, rotor3);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -30,24 +58,36 @@ namespace Enigma
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            rotor1 = Int32.Parse(comboBox1.Text);
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            rotor2 = Int32.Parse(comboBox2.Text);
-        }
-
-        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            rotor3 = Int32.Parse(comboBox3.Text);
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value == 27)
+                numericUpDown1.Value = 1;
+            if (numericUpDown1.Value == 0)
+                numericUpDown1.Value = 26;
+            //lblRotorI.Text = alpha.Substring(Int32.Parse((numericUpDown1.Value - 1).ToString()), 1);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown2.Value == 27)
+                numericUpDown2.Value = 1;
+            if (numericUpDown2.Value == 0)
+                numericUpDown2.Value = 26;
+            //lblRotorII.Text = alpha.Substring(Int32.Parse((numericUpDown2.Value - 1).ToString()), 1);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown3.Value == 27)
+                numericUpDown3.Value = 1;
+            if (numericUpDown3.Value == 0)
+                numericUpDown3.Value = 26;
+            //lblRotorIII.Text = alpha.Substring(Int32.Parse((numericUpDown3.Value - 1).ToString()), 1);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -55,15 +95,19 @@ namespace Enigma
 
         }
 
-
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if (class1.backspace == true)
+            EnigmaKeyDown(sender,e);
+        }
+
+        private void EnigmaKeyDown(object sender, EventArgs e)
+        {
+            textBox1.CharacterCasing = CharacterCasing.Upper;
+            if (backspace == true)
             {
-                if (textBox2.Text.Substring(textBox2.Text.Length - 1, 1) != " " && class1.ralpha.IsMatch(textBox2.Text.Substring(textBox2.Text.Length - 1, 1)))
+                if (textBox2.Text.Substring(textBox2.Text.Length - 1, 1) != " " && ralpha.IsMatch(textBox2.Text.Substring(textBox2.Text.Length - 1, 1)))
                     Decrement_Rotors();
-                class1.l--;
+                l--;
                 textBox2.Text = textBox2.Text.Substring(0, textBox2.Text.Length - 1);
                 return;
             }
@@ -73,32 +117,32 @@ namespace Enigma
                 textBox2.Text = "";
                 return;
             }
-            if (class1.l == textBox1.Text.Length)
+            if (l == textBox1.Text.Length)
             {
                 //If pasted value is same length
-                class1.l = 0;
+                l = 0;
                 textBox2.Text = "";
             }
-            if (class1.l + 1 < textBox1.Text.Length)
+            if (l + 1 < textBox1.Text.Length)
             {
                 //If pasted value is greater
-                class1.l = 0;
+                l = 0;
                 textBox2.Text = "";
             }
-            if (class1.l + 1 > textBox1.Text.Length)
+            if (l + 1 > textBox1.Text.Length)
             {
                 //If pasted value is less
-                class1.l = 0;
+                l = 0;
                 textBox2.Text = "";
             }
-            if (class1.l + 1 != textBox1.Text.Length)
+            if (l + 1 != textBox1.Text.Length)
             {
                 Geschichten();
-                int o = class1.l;
+                int o = l;
                 for (int x = 1; x <= textBox1.Text.Length - o; x++)
                 {
                     Encrypt();
-                    class1.l++;
+                    l++;
                 }
             }
             else//encrypt by one
@@ -107,7 +151,195 @@ namespace Enigma
                     Geschichten();
                 Encrypt();
             }
-            class1.l = textBox1.Text.Length;
+            l = textBox1.Text.Length;
         }
+
+        private void Geschichten()
+        {
+            //Geschichte
+            if (geschichte == 5)
+            {
+                ringgeschichte[0, 0] = ringgeschichte[1, 0];
+                ringgeschichte[0, 1] = ringgeschichte[1, 1];
+                ringgeschichte[0, 2] = ringgeschichte[1, 2];
+                ringgeschichte[0, 3] = ringgeschichte[1, 3];
+                ringgeschichte[1, 0] = ringgeschichte[2, 0];
+                ringgeschichte[1, 1] = ringgeschichte[2, 1];
+                ringgeschichte[1, 2] = ringgeschichte[2, 2];
+                ringgeschichte[1, 3] = ringgeschichte[2, 3];
+                ringgeschichte[2, 0] = ringgeschichte[3, 0];
+                ringgeschichte[2, 1] = ringgeschichte[3, 1];
+                ringgeschichte[2, 2] = ringgeschichte[3, 2];
+                ringgeschichte[2, 3] = ringgeschichte[2, 3];
+                ringgeschichte[3, 0] = ringgeschichte[4, 0];
+                ringgeschichte[3, 1] = ringgeschichte[4, 1];
+                ringgeschichte[3, 2] = ringgeschichte[4, 2];
+                ringgeschichte[3, 3] = ringgeschichte[4, 3];
+                geschichte--;
+            }
+            ringgeschichte[geschichte, 0] = (int)(numericUpDown1.Value);
+            ringgeschichte[geschichte, 1] = (int)(numericUpDown2.Value);
+            ringgeschichte[geschichte, 2] = (int)(numericUpDown3.Value);
+            //ringgeschichte[geschichte, 3] = (int)(numericUpDown4.Value);
+            geschichte++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            l = 0;
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 1;
+            numericUpDown2.Value = 1;
+            numericUpDown3.Value = 1;
+        }
+
+        private void Increment_Rotors()
+        {
+            iii++;
+            if (iii > 25)
+                iii = iii - 26;
+
+            if (ii == (notch2 - 1) || ii == (notch2second - 1))
+            {
+                i++;
+                ii++;
+            }
+            else
+            {
+
+                if (iii == notch3 || iii == notch3second)
+                {
+                    ii++;
+                }
+            }
+            if (ii > 25)
+                ii = ii - 26;
+
+            if (i > 25)
+                i = i - 26;
+        }
+
+        private void Decrement_Rotors()
+        {
+            iii--;
+            if (iii < 0)
+                iii = iii + 26;
+
+            if (ii == (notch2) || ii == notch3second)
+            {
+                i--;
+                ii--;
+            }
+            else
+            {
+                if (iii == (notch3 - 1) || iii == (notch2second - 1))
+                {
+                    ii--;
+                }
+            }
+            if (ii < 0)
+                ii = ii + 26;
+            if (i < 0)
+                i = i + 26;
+            numericUpDown1.Value = i + 1;
+            numericUpDown2.Value = ii + 1;
+            numericUpDown3.Value = iii + 1;
+        }
+
+        private void Encrypt()
+        {
+            shidden = "";
+            string sOutput = "";
+            string sInput = textBox1.Text.Substring(l, 1);
+            i = Int32.Parse(numericUpDown1.Value.ToString()) - 1;
+            ii = Int32.Parse(numericUpDown2.Value.ToString()) - 1;
+            iii = Int32.Parse(numericUpDown3.Value.ToString()) - 1;
+            //iv = Int32.Parse(numericUpDown4.Value.ToString()) - 1;
+
+            if (sInput == " ")
+            {
+                sOutput = " ";
+                textBox2.Text += " ";
+                return;
+            }
+            if (ralpha.IsMatch(sInput) == false)
+            {
+                sOutput = sInput;
+                textBox2.Text += sOutput;
+                return;
+            }
+            if (steckerbrett[0].Contains(sInput))
+            {
+                sOutput = steckerbrett[1].Substring(steckerbrett[0].IndexOf(sInput), 1);
+                shidden = sInput + "->" + sOutput;
+            }
+            else if (steckerbrett[1].Contains(sInput))
+            {
+                sOutput = steckerbrett[0].Substring(steckerbrett[1].IndexOf(sInput), 1);
+                shidden = sInput + "->" + sOutput;
+            }
+            else//Not a plugged character
+            {
+                shidden = sInput;
+                sOutput = sInput;
+            }
+            Increment_Rotors();//step on typing and before encryption
+
+            sOutput = rotor3.Encrypt(iii - ringstellung[2], sOutput);
+            shidden += sOutput;
+            sOutput = rotor2.Encrypt(ii - iii - ringstellung[1] + ringstellung[2], sOutput);
+            shidden += sOutput;
+            sOutput = rotor1.Encrypt(i - ii - ringstellung[0] + ringstellung[1], sOutput);
+            shidden += sOutput;
+
+            if (sRotor4 != "")
+            {
+                sOutput = rotor4.Encrypt(iv - ringstellung[3], sOutput);
+                shidden += sOutput;
+            }
+
+            shidden += "<-->";
+            sOutput = reflector1.Encrypt(ringstellung[0] - i, sOutput);
+            shidden += sOutput;
+
+            if (sRotor4 != "")
+            {
+                sOutput = rotor4.Decrypt(iv - ringstellung[3], sOutput);
+                shidden += sOutput;
+            }
+
+            sOutput = rotor1.Decrypt(i - ringstellung[0], sOutput);
+            shidden += sOutput;
+            sOutput = rotor2.Decrypt(ii - ringstellung[1], sOutput);
+            shidden += sOutput;
+            sOutput = rotor3.Decrypt(iii - ringstellung[2], sOutput);
+            shidden += sOutput;
+
+            if (steckerbrett[0].Contains(sOutput))
+            {
+                sOutput = steckerbrett[1].Substring(steckerbrett[0].IndexOf(sOutput), 1);
+                shidden += sInput + "->" + sOutput;
+            }
+            else if (steckerbrett[1].Contains(sOutput))
+            {
+                sOutput = steckerbrett[0].Substring(steckerbrett[1].IndexOf(sOutput), 1);
+                shidden += sInput + "->" + sOutput;
+            }
+
+            //numericUpDown4.Value = iv + 1;
+            numericUpDown3.Value = iii + 1;
+            numericUpDown2.Value = ii + 1;
+            numericUpDown1.Value = i + 1;
+            textBox2.Text += sOutput;
+            //tbxHidden.Text = shidden;
+        }
+
+
     }
 }
